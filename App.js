9df90661d1ff15d1b76913cbd6a5f40db97b258d
HenriqueDSCarvalho <h96352828@gmail.com>
2022-03-11 21:34:31 -0300

    prototipo primeira tela

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Icon } from 'react-native';
 
 
 
@@ -7,16 +7,35 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Anotações</Text>
-      <StatusBar style="auto" />
 
-      <Button
-      title='pesquisa'
-      color= '#ff00ff'
-      accessibilityLabel='label'
-      
+      <View style={styles.areaTopo}>      
+      <Image 
+      style={styles.imgBarra}
+      source={require('./src/img/OIP.jpg')}
+      />
+      <TextInput
+        style={styles.input}
+        keyboardType= "name-phone-pad"
       />
 
+
+      <Image
+      style={styles.imgPesquisa}
+      source={require('./src/img/R.png')}
+      />
+      </View>
+      <StatusBar style="auto" />   
+
+
+
+      <View style={styles.viewPesquisa}>
+      <Image
+      style={styles.imgAdc}
+      source={require('./src/img/adicao.png')}
+      
+      />
+      </View>  
+     
 
 
      
@@ -27,9 +46,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+   
   },
+  areaTopo:{
+    paddingHorizontal: 20,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+    
+   },
+  imgBarra:{
+    width: 40,
+    height: 40,
+    padding: 20,
+    
+    
+
+  },
+  input: {
+    height: 30,
+    margin: 5,
+    borderWidth: 1,
+    paddingHorizontal: 70
+  },
+  imgPesquisa:{
+    width: 40,
+    height: 40,
+    padding: 20,
+  
+  },
+  viewPesquisa:{
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    paddingTop: 660,
+    paddingRight: 50
+    
+  },
+  imgAdc:{
+    width: 40,
+    height: 40,
+    
+    
+  }
+  
  
 });
